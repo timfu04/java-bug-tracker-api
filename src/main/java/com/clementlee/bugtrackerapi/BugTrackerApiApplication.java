15ef67e9255a72c8f30fe085d6c5bc91bd7c6166
Clement <clementlee1999@gmail.com>
2023-05-22 19:34:01 +0800

    first commit

---
+++ src/main/java/com/clementlee/bugtrackerapi/BugTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.clementlee.bugtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerApiApplication.class, args);
+	}
+
+}
